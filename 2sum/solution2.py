124af41488d237b8930de8a75434e1b804ec99de
yongle <lyuyongle@gmail.com>
2016-03-05 23:22:25 -0600

    use class

--- 2sum/solution2.py
+++ 2sum/solution2.py
@@ -1,25 +1,28 @@
 # use binary search for sorted array
-def bsearch(alist, item):
-	#return index of item in alist, none if not found
-	left, right = 0, len(alist)-1
-	while left <= right:
-		mid = (left + right)/2
-		if alist[mid] == item:
-			return mid + 1 # not zero based this problem
-		elif alist[mid] > item:
-			right = mid -1
-		else:
-			left = mid + 1
+class Solution(object):
 
-def twoSum(num, target):
-	for i in range(len(num)):
-		rest = target - num[i]
-		search = bsearch(num[i+1:], rest)
-		if search == None:
-			continue
-		else:
-			return i + 1, i + 1 + search
+	def bsearch(self,alist, item):
+		#return index of item in alist, none if not found
+		left, right = 0, len(alist)-1
+		while left <= right:
+			mid = (left + right)/2
+			if alist[mid] == item:
+				return mid + 1 # not zero based this problem
+			elif alist[mid] > item:
+				right = mid -1
+			else:
+				left = mid + 1
+
+	def twoSum(self, num, target):
+		for i in range(len(num)):
+			rest = target - num[i]
+			search = self.bsearch(num[i+1:], rest)
+			if search == None:
+				continue
+			else:
+				return i + 1, i + 1 + search
 
 num = [1, 3, 5, 6, 7]
 target = 7
-print twoSum(num, target) #(1, 4)
+test = Solution()
+print test.twoSum(num, target) #(1, 4)
