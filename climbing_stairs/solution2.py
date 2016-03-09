646c0e1d00f87e61026a8cc8bb8acd15177ccde7
yongle <lyuyongle@gmail.com>
2016-03-08 18:14:28 -0600

    climbing stairs iterate

--- climbing_stairs/solution2.py
+++ climbing_stairs/solution2.py
@@ -6,13 +6,15 @@ Each time you can either climb 1 or 2 steps.
 In how many distinct ways can you climb to the top?
 """
 class Solution(object):
+	# time O(n); space O(1)
 	def climb_stairs(self,n):
-		# use recursive
-		if n in [1,2]:
-			return n 
-		steps = self.climb_stairs(n-1) + self.climb_stairs(n-2)
-		return steps
+		# iterate fibonacci
+		# dp
+		dp = [1 for i in range(n+1)]
+		for i in range(2,n+1):
+			dp[i] = dp[i-1] + dp[i-2]
+		return dp[n]
 
-n = 5
+n = 2
 s = Solution()
-print s.climb_stairs(n) #5
+print s.climb_stairs_1(n) #8
